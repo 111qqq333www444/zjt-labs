8d2f30d081fe4d3347685e77119fef1af6f90aa2
111qqq333www444 <3644951174@qq.com>
2026-09-19 16:52:38 +0000

    更新API隧道地址

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * API 服务器地址（隧道域名会变化，改这里即可）
  */
 window.ZJT_CONFIG = {
-    API_BASE: 'https://f0ba3037b2f872.lhr.life',
+    API_BASE: 'https://7e64f29ec7f42b.lhr.life',
     APP_NAME: '证件通 · 管理录入系统',
     MAX_PHOTO_MB: 10
 };
